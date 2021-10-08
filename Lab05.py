b5c8dd368ee95125b007d3b79e912902d1de6e2e
realmattclark <matt.clark5991@gmail.com>
2021-10-07 20:29:36 -0500

    Update Lab05.py

--- Lab05.py
+++ Lab05.py
@@ -2,6 +2,16 @@
 #CS101 Lab
 #6 October 2021
 
+
+#ALGORITHM:
+# Ask user for chips, wager and then run the program
+
+#Error:
+# Wager not less than 0 or more than 100
+
+
+
+
 import random
 
 def play_again():
@@ -14,75 +24,81 @@ def play_again():
             return False
         else: print('Must answer with "y" or "n"')
 
-def wager():
+def get_wager(bank : int):
     while True:
         global wager
         
         wager = int(input('Enter your wager: \n'))
-        if wager <= 0 or wager > bank['money']:
+        if wager <= 0 or wager > bank:
             print('Your value must be greater than zero and less than the current chips in your bank')
         else:
             return wager
 
-def slot_results():
-    global reel1
-    global reel2
-    global reel3
-    reel1 = random.randint(1,10)
-    reel2 = random.randint(1,10)
-    reel3 = random.randint(1,10)
+def get_slot_results():
+    global reela
+    global reelb
+    global reelc
+    
+    reela = random.randint(1,10)
+    reelb = random.randint(1,10)
+    reelc = random.randint(1,10)
 
-    results = [reel1, reel2, reel3]
-    print(reel1, reel2, reel3)
+    results = [reela, reelb, reelc]
     return results
 
-def get_matches(reel1, reel2, reel3):
+def get_matches(reela, reelb, reelc):
     global matches
     matches = 0
-    if reel1 == reel2 and reel2 == reel3:
+    if reela == reelb and reelb == reelc:
         matches = 3
-    if reel1 == reel2 and reel2 != reel3:
+    if reela == reelb and reelb != reelc:
         matches = 2
     else:
         matches = 0
-    print(matches, 'matches.\n')
+    print(matches, ' matches.\n')
     return matches
 
 def get_bank():
-    while True:
-        chips = int(input('How many chips would you like to start with? : \n'))
-        if chips < 1 or chips > 100:
+        global bank
+        bank = int(input('How many chips would you like to start with? : \n'))
+        if bank < 1 or bank > 100:
             print('Please enter a value between 1 and 100')
         else:
-            bank['money'] = chips
-            return chips
+            return bank
 
-def payout(matches, payout):
+def get_payout(wager, matches):
     payout = 0
     if matches == 3:
-        payout = (wager * 10) - wager
+        payout = wager * 10
     if matches == 2:
-        payout = (wager * 3) - wager
+        payout = wager * 3
     if matches == 0:
         payout = bank - wager
-    return payout
+    return wager * -1
 
-bank = {'money' : 0}
-print('Welcome to the slot machine!')
-get_bank()
-while True:
-    wager()
-    slot_results()
-    get_matches(reel1, reel2, reel3)
-    outcome = payout(wager, matches)
-    bank['money'] += outcome
-    print('Current bank balance :', bank['money'])
-    if bank['money'] > 0:
-        if play_again() == False:
-            print('Thank you for playing!')
-            quit()
-        else:
-            continue
-    else:
-        print('You do not have any more money to play with. Thank you!')
-        quit()
+if __name__ == "__main__":
+
+    playing = True
+    while playing:
+
+        bank = get_bank()
+
+        while True: 
+            
+            wager = get_wager(bank)
+
+            reela, reelb, reelc = get_slot_results()
+
+            matches = get_matches(reela, reelb, reelc)
+            payout = get_payout(wager, matches)
+            bank = bank + payout
+
+            print("Your spin", reela, reelb, reelc)
+            print("You matched", matches, "reels")
+            print("You won/lost", payout)
+            print("Current bank", bank)
+            print()
+           
+    print("You lost all", 0, "in", 0, "spins")
+    print("The most chips you had was", 0)
+    playing = play_again()
